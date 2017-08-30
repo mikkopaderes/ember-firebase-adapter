ef24615bf571c603afff27398eb45614b0caa4b4
Mikko Paderes <rmmpaderes@gmail.com>
2017-08-30 17:32:44 +0800

    Initial setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,12 +2,45 @@ module.exports = {
   root: true,
   parserOptions: {
     ecmaVersion: 2017,
-    sourceType: 'module'
+    sourceType: 'module',
+    ecmaFeatures: {
+      'experimentalObjectRestSpread': true,
+    },
   },
-  extends: 'eslint:recommended',
+  extends: ['eslint:recommended', 'google'],
   env: {
-    browser: true
+    browser: true,
+    es6: true,
   },
   rules: {
-  }
+    // -------------------------------------------
+    // Google config overrides due to Ember issues
+    // -------------------------------------------
+
+    // Causes errors in tests due to this being thrown around
+    'no-invalid-this': 'off',
+
+    // ---------------
+    // Personal config
+    // ---------------
+
+    'comma-dangle': ['error', {
+      arrays: 'always-multiline',
+      objects: 'always-multiline',
+      imports: 'always-multiline',
+      exports:'always-multiline',
+      functions: 'always-multiline',
+    }],
+    'max-len': ['error', {
+      code: 80,
+      tabWidth: 2,
+      ignoreUrls: true,
+      ignoreStrings: true,
+      ignoreTemplateLiterals: true,
+      ignoreRegExpLiterals: true,
+    }],
+    'object-curly-spacing': ['error', 'always'],
+    'arrow-body-style': ['error', 'as-needed'],
+    'prefer-const': 'error',
+  },
 };
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # ember-firebase-adapter
 
-This README outlines the details of collaborating on this Ember addon.
+Unofficial Ember Adapter and Serializer for Firebase
 
 ## Installation
 
